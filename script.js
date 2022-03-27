ee456fe29119e3e5271adb93bf8ab435e17db4c2
Maltiti Tahiru <mtahiru52@gmail.com>
2022-03-26 18:04:45 -0700

    updatr js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // global constants
-
 const cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 //const roundsToPlay = 6; //how many rounds will be played
